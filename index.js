d36f43e4b8cf2d6b81d7738a2e27a481a4d7bbbd
Desh Bagley <stemtrainer@gmail.com>
2022-03-25 12:14:51 -0400

    assignment done

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
+  if (distance <= 400){
+    return "This one is on me!"
+  }
+  else if (distance >2500){
+    return "No can do."
+  }
+  else return "I will gladly take your thirty bucks."
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  return (city === "NYC" ? "Ok, sounds good." : "No go.");
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  // Write your switch code here
+  switch (tip) {
+    case "generous" : return "Thank you so much."
+    case "not as generous" : return "Thank you."
+    default : return "Bye."
+  }
 }
